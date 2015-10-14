c3e7e696a8fbf35f9e97328a61b0029a27cc0041
Xuyi@Linux_VM <xruan@wisc.edu>
2015-10-14 00:13:25 -0400

    fana

--- lab1/switchyard/examples/exercises/learning_switch/myswitch.py
+++ lab1/switchyard/examples/exercises/learning_switch/myswitch.py
@@ -10,6 +10,7 @@ that doesn't learn.)
 from switchyard.lib.address import *
 from switchyard.lib.packet import *
 from switchyard.lib.common import *
+import threading
 
 def switchy_main(net):
     my_interfaces = net.interfaces() 
@@ -20,7 +21,7 @@ def switchy_main(net):
     # - intf.ethaddr -> mac address
     # - intf.name -> port/interface (eth0, eth1, etc)
     # - intf.netmask - network mask associated with the IPv4 address
-    mymacs = [intf.ethaddr.toStr() for intf in my_interfaces]
+    #mymacs = [intf.ethaddr.toStr() for intf in my_interfaces]
     
     # ?? intf.ethaddr is a ethaddr instance [Ethernet (ixx,xx,xx,x,x)]
     ''' 
@@ -28,16 +29,22 @@ def switchy_main(net):
         print(dir(intf.ethaddr), exit())
     '''
 
-    print ("mymacs: ", mymacs)
+    #print ("mymacs: ", mymacs)
     # TODO: construct a dict with <ethaddr : port> pair
-    forwardList = dict([(intf.ethaddr.toStr(), intf.name) for intf in my_interfaces])
-    print ("forwardList", forwardList)
+    forwardList = dict([(intf.ethaddr.toStr(), [intf.name, "time"]) for intf in my_interfaces])
+    #print ("forwardList", forwardList)
+    
+    # call timeout functiion
+    timeout()
+
     while True:
         try:
             # dev -> device name
             # packet -> packet receive
             dev,packet = net.recv_packet()
         except NoPackets:
+            forwardList = dict()
+            #mymacs = []
             continue
         except Shutdown:
             return
@@ -46,26 +53,32 @@ def switchy_main(net):
         # log_debug ("I am here!")
         # TODO: if the source MAC addr. not seen before, add it to the forward table.
         srcAddr = packet[0].src.toStr()
-        if srcAddr not in mymacs:
-            print ("src addr. not in forward table, add", srcAddr , "to table")
-            forwardList[srcAddr] = dev
+        if srcAddr not in forwardList:
+            #print ("src addr. not in forward table, add", srcAddr , "to table")
+            forwardList[srcAddr] = [dev, "time"]
             # update mymacs list
-            mymacs.append(srcAddr)
-        print ("src: ", packet[0].src)
-        print ("dst: ", packet[0].dst)
-        print ("forwardList: ", forwardList)
-        if packet[0].dst.toStr() in mymacs:
+            #mymacs.append(srcAddr)
+        #print ("src: ", packet[0].src)
+        #print ("dst: ", packet[0].dst)
+        #print ("forwardList: ", forwardList)
+        if packet[0].dst.toStr() in forwardList:
             print ("packet with dst: ", packet[0].dst, " recognized by forwrad table")
             # packet[0] - header for the packet
             # other data in the header: dst, src, ethertype
             log_debug ("Packet intended for me")
             # TODO: look up port for dst addr. and forward the packet to that port.
-            print ("send packet to ", forwardList[packet[0].dst.toStr()], "interface")
-            net.send_packet(forwardList[packet[0].dst.toStr()], packet)
+            #print ("send packet to ", forwardList[packet[0].dst.toStr()][0], "interface")
+            net.send_packet(forwardList[packet[0].dst.toStr()][0], packet)
         else:
-            print ("packet with dst: ", packet[0].dst, " NOT recognized by forwrad table. broadcast the packet")
+            #print ("packet with dst: ", packet[0].dst, " NOT recognized by forwrad table. broadcast the packet")
             for intf in my_interfaces:
                 if dev != intf.name:
                     log_debug ("Flooding packet {} to {}".format(packet, intf.name))
                     net.send_packet(intf.name, packet)
     net.shutdown()
+
+
+def timeout():
+    t = threading.Timer(2.0, timeout).start()
+    print ("timeout: ", switchy_main.forwardList)
+
