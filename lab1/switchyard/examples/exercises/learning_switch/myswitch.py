a36296bf47f4f6db52c663c8421bc1f959ebcd77
Xuyi@Linux_VM <xruan@wisc.edu>
2015-10-19 21:57:34 -0500

    Updated: added test file

--- lab1/switchyard/examples/exercises/learning_switch/myswitch.py
+++ lab1/switchyard/examples/exercises/learning_switch/myswitch.py
@@ -23,7 +23,6 @@ def switchy_main(net):
     forwardList = dict([(intf.ethaddr.toStr(), [intf.name, currnt_time_in_sec()+30]) for intf in my_interfaces]) 
     # call timeout functiion
     timeout()
-    #print ("main thread: ", threading.get_ident())
     while True:
         try:
             # dev -> device name
@@ -36,25 +35,20 @@ def switchy_main(net):
 
         log_debug ("In {} received packet {} on {}".format(net.name, packet, dev))
         # TODO: if the source MAC addr. not seen before, add it to the forward table.
-        #print ("main thread: ", threading.get_ident())
         srcAddr = packet[0].src.toStr()
         if srcAddr not in forwardList:
             forwardList[srcAddr] = [dev, currnt_time_in_sec()+30]
         if packet[0].dst.toStr() in forwardList:
-            #print ("packet with dst: ", packet[0].dst, " recognized by forwrad table")
             # packet[0] - header for the packet
             # other data in the header: dst, src, ethertype
             log_debug ("Packet intended for me")
             # TODO: look up port for dst addr. and forward the packet to that port.
-            #print ("send packet to ", forwardList[packet[0].dst.toStr()][0], "interface")
             net.send_packet(forwardList[packet[0].dst.toStr()][0], packet)
         else:
-            #print ("packet with dst: ", packet[0].dst, " NOT recognized by forwrad table. broadcast the packet")
             for intf in my_interfaces:
                 if dev != intf.name:
                     log_debug ("Flooding packet {} to {}".format(packet, intf.name))
                     net.send_packet(intf.name, packet)
-    # exit = True
     net.shutdown()
 
 def timeout():
@@ -62,7 +56,6 @@ def timeout():
     # clean stale entries
     update_List = {}
     for key, value in forwardList.items():
-        #print ("entry: ", key, "value: ", value)
         if (value[1] > currnt_time_in_sec()):
             update_List[key] = value
     forwardList = update_List
@@ -71,11 +64,8 @@ def timeout():
         # if main fucntion still alive
         time_thread = threading.Timer(2.0, timeout)
         time_thread.start()
-        #print ("start new thread: ", threading.current_thread())
     else: 
-        #print ("cancel current thread: ", threading.current_thread())
         time_thread.cancel()
-    #print ("timeout: ", forwardList)
 
 # code soure for get current time, please refer to README.md
 def currnt_time_in_sec():
---
+++ lab1/switchyard/switch_test.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import sys
+from switchyard.lib.address import *
+from switchyard.lib.packet import *
+from switchyard.lib.common import *
+from switchyard.lib.testing import *
+
+def mk_pkt(hwsrc, hwdst, ipsrc, ipdst, reply=False):
+    ether = Ethernet()
+    ether.src = EthAddr(hwsrc)
+    ether.dst = EthAddr(hwdst)
+    ether.ethertype = EtherType.IP
+
+    ippkt = IPv4()
+    ippkt.srcip = IPAddr(ipsrc)
+    ippkt.dstip = IPAddr(ipdst)
+    ippkt.protocol = IPProtocol.ICMP
+    ippkt.ttl = 32
+
+    icmppkt = ICMP()
+    if reply:
+        icmppkt.icmptype = ICMPType.EchoReply
+    else:
+        icmppkt.icmptype = ICMPType.EchoRequest
+
+    return ether + ippkt + icmppkt
+
+def switch_tests():
+    s = Scenario("switch tests")
+    s.add_interface('eth0', '10:00:00:00:00:01')
+    s.add_interface('eth1', '10:00:00:00:00:02')
+    s.add_interface('eth2', '10:00:00:00:00:03')
+
+
+    # test case 1: a frame from A to D (f_table not learned dest addr yet)
+    # all ports except ingress
+    testpkt = mk_pkt("00:00:00:00:00:aa", "00:00:00:00:00:dd", "10.0.0.1", "10.0.0.4")
+    s.expect(PacketInputEvent("eth0", testpkt, display=Ethernet), "An Ethernet frame fomr 00:00:00:00:00:aa to 00:00:00:00:00:dd should arrive on eth0")
+    s.expect(PacketOutputEvent("eth1", testpkt, "eth2", testpkt, display=Ethernet), "The Ethernet frame for 00:00:00:00:00:dd should be flouded out eth1 and eth2")
+    
+    # timeout for 20 sec
+    s.expect(PacketInputTimeoutEvent(20.0), "Time out for 20 seconds")
+    
+    # test case 2: a frame from D to A (learned)
+    testpkt = mk_pkt("00:00:00:00:00:dd", "00:00:00:00:00:aa", "10.0.0.4", "10.0.0.1")
+    s.expect(PacketInputEvent("eth2", testpkt, display=Ethernet), "An Ethernet frame from 00:00:00:00:00:dd should arrive on eth2")
+    s.expect(PacketOutputEvent("eth0", testpkt, display=Ethernet), "The Ethernet frame for 00:00:00:00:00:aa should be flouded on eth0")
+
+    # test case 3: a frame from C to boardcast
+    reqpkt = mk_pkt("00:00:00:00:00:cc", "ff:ff:ff:ff:ff:ff", "10.0.0.3", "255.255.255.255")
+    s.expect(PacketInputEvent("eth1", reqpkt, display=Ethernet), "An Ethernet frame from 00:00:00:00:00:cc with a broadcast destination address should arrive on eth1")
+    s.expect(PacketOutputEvent("eth0", reqpkt, "eth2", reqpkt, display=Ethernet), "The Ethernet frame with a broadcast destination address should be forward out ports eth0 and eth2")
+
+    # timeout for 10 sec
+    s.expect(PacketInputTimeoutEvent(15.0), "Time out for 10 seconds")
+    
+    # test case 4: a frame from C to A (learned, but forget)
+    testpkt = mk_pkt("00:00:00:00:00:cc", "00:00:00:00:00:aa", "10.0.0.3", "10.0.0.1")
+    s.expect(PacketInputEvent("eth1", testpkt, display=Ethernet), "An Ethernet frame from 00:00:00:00:00:dd should arrive on eth2")
+    s.expect(PacketOutputEvent("eth0", testpkt, "eth2", testpkt, display=Ethernet), "The Ethernet frame for 00:00:00:00:00:aa should be flouded on eth0")
+
+    # test case 5: a frame from A to C (learned)
+    testpkt = mk_pkt("00:00:00:00:00:aa", "00:00:00:00:00:cc", "10.0.0.1", "10.0.0.3")
+    s.expect(PacketInputEvent("eth0", testpkt, display=Ethernet), "An Ethernet frame from 00:00:00:00:00:aa should arrive on eth0")
+    s.expect(PacketOutputEvent("eth1", testpkt, display=Ethernet), "The Ethernet frame for 00:00:00:00:00:cc should be flouded on eth1")
+
+    return s
+
+scenario = switch_tests()
